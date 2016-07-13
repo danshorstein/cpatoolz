1ada0cd959a87ba6864c880d25a16854c6ff3cc2
dtiz <dshorstein@gmail.com>
2016-07-13 16:44:28 -0400

    Release 0.0
    lalala

--- setup.py
+++ setup.py
@@ -3,17 +3,10 @@
 import os
 from setuptools import setup
 
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
-
 readme = os.path.join(os.path.dirname(__file__), 'README.md')
 setup(
     name='bottomline',
-    description='an accountant\'s toolbox',
+    description='an accountants toolbox',
     long_description=read_md(readme) if os.path.exists(readme) else '',
     version='0.0',
     packages=['bottomline'],
@@ -21,7 +14,7 @@ setup(
     author='Daniel Shorstein',
     author_email='dshorstein@gmail.com',
     url='https://github.com/dtiz/bottomline',
-    install_requires=[],
+    install_requires=['pandas'],
     entry_points=dict(
         console_scripts=[
             'bottomline = bottomline:main',
