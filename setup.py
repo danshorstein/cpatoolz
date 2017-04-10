0782c0899532de6426afed229a4a31a032405fad
Daniel Shorstein <dshorstein@gmail.com>
2017-04-10 16:13:21 -0400

    more stuff

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 
 readme = os.path.join(os.path.dirname(__file__), 'README.md')
 setup(
-    name='cpatoolz',
+    name='simple_ledger',
     description='an accountants toolbox',
     version='0.0',
-    packages=['cpatoolz'],
+    packages=['simple_ledger'],
     license='The MIT License',
     author='Daniel Shorstein',
     author_email='dshorstein@gmail.com',
-    url='https://github.com/danshorstein/cpatoolz',
+    url='https://github.com/danshorstein/simple_ledger',
     install_requires=['pandas'],
     entry_points=dict(
         console_scripts=[
