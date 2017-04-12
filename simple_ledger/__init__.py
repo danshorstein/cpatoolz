a40d8462d7535e8758d2f50255e9d9a2d3d5a528
Daniel Shorstein <dshorstein@gmail.com>
2017-04-12 17:51:59 -0400

    #progress

--- simple_ledger/__init__.py
+++ simple_ledger/__init__.py
@@ -1,29 +1,37 @@
 from simple_ledger.api import GeneralLedgerService
+from datetime import datetime
 
 
 def main():
+    beg_date = datetime(2018, 1, 15)
+    end_date = datetime(2018, 5, 15)
+    accts = [1000, 1010, 1200, 2000, 2110, 4000, 5010]
+
     gl = GeneralLedgerService()
 
     gl.load_detail()
 
     gl.validate()
 
+    print('')
     print('Chart of accounts:')
     print(gl.chart_of_accts())
 
-    print(gl)
+    # print('')
+    # print('Filtered for accts: {}'.format(accts))
+    # print(gl.filter_detail(accounts=accts))
 
     print('')
-    print('Filtered for XXXXX')
-    print(gl.filter_detail())
+    print('Summarized for date range {} to {} and accts {}'.format(beg_date.date(), end_date.date(), accts))
+    print(gl.sum_by_gl_account(accounts=accts, beginning_date=beg_date, ending_date=end_date))
 
     print('')
-    print('Summarized for date range XXXX - XXXX')
-    print(gl.summary())
+    print('Summaried FS grouping for range {} to {}'.format(beg_date.date(), end_date.date()))
+    print(gl.sum_by_fs_grouping(beginning_date=beg_date, ending_date=end_date))
 
     print('')
-    print('Summaried FS grouping for range XXXX - XXXX')
-    print(gl.fs_summary())
+    print('Dataframe info: ')
+    gl.df.info()
 
 if __name__ == '__main__':
     main()
--- simple_ledger/api.py
+++ simple_ledger/api.py
@@ -11,48 +11,67 @@ class GeneralLedgerService:
         self.acct_number = "AcctNum"
         self.acct_name = 'Acct'  # TODO - update this to properly include the name of the account column
         self.net = 'Net'
+        self.date = 'Date'
         self.validation = {'validation_issues': 0}
         self.base_folder = os.path.abspath(os.path.dirname(__file__))
         self.csv_path = os.path.join(self.base_folder, 'data', '{}.csv'.format(self.user))
-        self.gl = pd.DataFrame()
+        self.df = pd.DataFrame()
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return str(self.gl)
+        return str(self.df)
 
     def load_detail(self):
-        self.gl = pd.read_csv(self.csv_path)
+        self.df = pd.read_csv(self.csv_path)
+        self.df[self.date] = self.df[self.date].apply(pd.to_datetime)
         print('Loaded gl detail from {}'.format(self.csv_path))
 
-    def summary(self, beg_date = None, end_date = None):
-        summary = pd.pivot_table(self.gl, values=self.net, index=[self.acct_number, self.acct_name], aggfunc='sum')
+    def sum_by_gl_account(self, accounts=None, beginning_date=None, ending_date=None):
+        filter_df = self.filter_detail(accounts=accounts, beginning_date=beginning_date, ending_date=ending_date)
+        summary = pd.pivot_table(filter_df, values=self.net, index=[self.acct_number, self.acct_name], aggfunc='sum')
         return summary
 
-    def fs_summary(self, beg_date = None, end_date = None):
-        summary = pd.pivot_table(self.gl, values=self.net, index=self.fs_group, aggfunc='sum')
+    def sum_by_fs_grouping(self, beginning_date=None, ending_date=None):
+        filter_df = self.filter_detail(beginning_date=beginning_date, ending_date=ending_date)
+        summary = pd.pivot_table(filter_df, values=self.net, index=self.fs_group, aggfunc='sum')
         return summary
 
     def validate(self):  # TODO - implement validations below
         # no null values
         # required columns exist
         # correct format in each column's data
-        net_sum = round(self.gl[self.net].sum(), 2)
+        net_sum = round(self.df[self.net].sum(), 2)
         if net_sum != 0:
             self.validation['validation_issues'] += 1
             self.validation['net_sum'] = net_sum
 
         return self.validation
 
-    def filter_detail(self, acct_numbers=None, beginning_date=None, ending_date=None):
-        pass
+    def filter_detail(self, accounts=None, beginning_date=None, ending_date=None):
+        if not beginning_date:
+            beginning_date = min(self.df[self.date])
+        if not ending_date:
+            ending_date = max(self.df[self.date])
+        if not accounts:
+            accounts = self.df[self.acct_number].unique()
+
+        filter_df = self.df.where(
+            self.df[self.acct_number].isin(accounts)
+        ).where(
+            self.df[self.date] >= beginning_date
+        ).where(
+            self.df[self.date] <= ending_date
+        ).dropna()
+
+        return filter_df
 
     def chart_of_accts(self):
-        coa = self.gl[self.acct_name].unique()  # TODO - ADD ACCT NUMBER AND ACCT DESCRIPTION
-        return coa
+        coa = self.df.groupby(by=[self.acct_number, self.acct_name]).max()  # TODO - ADD ACCT NUMBER AND ACCT DESCRIPTION
+        return coa[self.fs_group]
 
     def save_detail(self):
-        self.gl.to_csv(self.csv_path)
+        self.df.to_csv(self.csv_path, index=False)
         print('Saved file at {}'.format(self.csv_path))
 
