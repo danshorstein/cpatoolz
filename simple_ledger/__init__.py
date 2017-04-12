a9dcb10b4bbbf0201b5b79a92c39d9cf4b6d0adb
Daniel Shorstein <dshorstein@gmail.com>
2017-04-12 16:15:08 -0400

    #progress

--- simple_ledger/__init__.py
+++ simple_ledger/__init__.py
@@ -6,13 +6,24 @@ def main():
 
     gl.load_detail()
 
-    errors = gl.validate()
+    gl.validate()
 
-    if errors:
-        print('Errors: {}'.format(errors))
-
+    print('Chart of accounts:')
     print(gl.chart_of_accts())
 
+    print(gl)
+
+    print('')
+    print('Filtered for XXXXX')
+    print(gl.filter_detail())
+
+    print('')
+    print('Summarized for date range XXXX - XXXX')
+    print(gl.summary())
+
+    print('')
+    print('Summaried FS grouping for range XXXX - XXXX')
+    print(gl.fs_summary())
 
 if __name__ == '__main__':
     main()
--- simple_ledger/api.py
+++ simple_ledger/api.py
@@ -6,19 +6,32 @@ import os
 class GeneralLedgerService:
     def __init__(self, user='sample_gl'):
         # TODO - for now the user is the filename of the csv. This should be updated if a database is implemented...
+        self.fs_group = 'FS_Group'
         self.user = user
-        self.acct = 'Acct'  # TODO - update this to properly include the name of the account column
+        self.acct_number = "AcctNum"
+        self.acct_name = 'Acct'  # TODO - update this to properly include the name of the account column
         self.net = 'Net'
-        self.error = None
+        self.validation = {'validation_issues': 0}
+        self.base_folder = os.path.abspath(os.path.dirname(__file__))
+        self.csv_path = os.path.join(self.base_folder, 'data', '{}.csv'.format(self.user))
+        self.gl = pd.DataFrame()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return str(self.gl)
 
     def load_detail(self):
-        base_folder = os.path.abspath(os.path.dirname(__file__))
-        csv_path = os.path.join(base_folder, 'data', '{}.csv'.format(self.user))
-        self.gl = pd.read_csv(csv_path)
-        print('Loaded gl detail from {}'.format(csv_path))
+        self.gl = pd.read_csv(self.csv_path)
+        print('Loaded gl detail from {}'.format(self.csv_path))
 
-    def summary(self):
-        summary = pd.pivot_table(self.gl, values=self.net, index=self.acct, aggfunc='sum')
+    def summary(self, beg_date = None, end_date = None):
+        summary = pd.pivot_table(self.gl, values=self.net, index=[self.acct_number, self.acct_name], aggfunc='sum')
+        return summary
+
+    def fs_summary(self, beg_date = None, end_date = None):
+        summary = pd.pivot_table(self.gl, values=self.net, index=self.fs_group, aggfunc='sum')
         return summary
 
     def validate(self):  # TODO - implement validations below
@@ -27,12 +40,19 @@ class GeneralLedgerService:
         # correct format in each column's data
         net_sum = round(self.gl[self.net].sum(), 2)
         if net_sum != 0:
-            self.error = "GL detail doesn't net to 0! Balance is {}".format(net_sum)
-        return self.error
+            self.validation['validation_issues'] += 1
+            self.validation['net_sum'] = net_sum
 
-    def filter_detail(self, acct_number=None, beginning_date=None, ending_date=None):
+        return self.validation
+
+    def filter_detail(self, acct_numbers=None, beginning_date=None, ending_date=None):
         pass
 
     def chart_of_accts(self):
-        coa = self.gl[self.acct].unique()  # TODO - ADD ACCT NUMBER AND ACCT DESCRIPTION
+        coa = self.gl[self.acct_name].unique()  # TODO - ADD ACCT NUMBER AND ACCT DESCRIPTION
         return coa
+
+    def save_detail(self):
+        self.gl.to_csv(self.csv_path)
+        print('Saved file at {}'.format(self.csv_path))
+
